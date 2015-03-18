59ac1dd2a8f6f72190046ecc89bb5f3fbb053049
Danack <Danack@basereality.com>
2015-03-18 12:16:23 +0000

    Tweaked box because of Chrome wanting all the memory.
    Former-commit-id: 67354c45073526d66e757d643b637990f31a0f3c

--- conf/chef/cookbooks/centos/recipes/default.rb
+++ conf/chef/cookbooks/centos/recipes/default.rb
@@ -6,21 +6,21 @@ execute 'centos_disable_iptables' do
     command  "service iptables stop"
 end
 
-bash "centos_alias_php" do
-    user "root"
-    cwd "/tmp"
-    code <<-EOT
-       echo "alias php='/usr/local/bin/php'" >> /root/.bashrc
-    EOT
-    not_if "grep /root/.bashrc /usr/local/bin/php"
-end
+#bash "centos_alias_php" do
+#    user "root"
+#    cwd "/tmp"
+#    code <<-EOT
+#       echo "alias php='/usr/local/bin/php'" >> /root/.bashrc
+#    EOT
+#    not_if "grep /root/.bashrc /usr/local/bin/php"
+#end
 
-bash "centos_alias_php-config" do
-    user "root"
-    cwd "/tmp"
-    code <<-EOT
-       echo "alias php-config='/usr/local/bin/php-config'" >> /root/.bashrc
-    EOT
-    not_if "grep /root/.bashrc /usr/local/bin/php"
-end
+#bash "centos_alias_php-config" do
+#    user "root"
+#    cwd "/tmp"
+#    code <<-EOT
+#       echo "alias php-config='/usr/local/bin/php-config'" >> /root/.bashrc
+#    EOT
+#    not_if "grep /root/.bashrc /usr/local/bin/php"
+#end
 
